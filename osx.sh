be0a79384d9f90cc00254f1e46a2c89b9e6a65bc
Kevin Huck <khuck@cs.uoregon.edu>
2023-02-16 12:07:30 -0800

    adding osx.sh

---
+++ osx.sh
@@ -0,0 +1,17 @@
+export CXX=`which clang++`
+export CC=`which clang`
+export APEX_ROOT=$HOME/src/apex/install
+
+# clean up
+rm -rf build
+
+# Configure
+cmake -B build \
+
+#-DCMAKE_CXX_COMPILER=`which amdclang++` \
+#-DCMAKE_C_COMPILER=`which amdclang` \
+#-DAPEX_ROOT=$HOME/src/xpress-apex/install_gilgamesh_5.2.0
+
+# Build
+cmake --build build -j
+ctest -V --test-dir build
