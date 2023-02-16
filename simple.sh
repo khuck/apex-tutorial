0efe67a5b406bb52ca5409624262120ad21655e7
Kevin Huck <khuck@cs.uoregon.edu>
2023-02-16 15:18:25 -0800

    Making kokkos optional

---
+++ simple.sh
@@ -0,0 +1,18 @@
+export CXX=`which amdclang++`
+export CC=`which amdclang`
+export APEX_ROOT=$HOME/src/xpress-apex/install_gilgamesh_5.2.0
+
+# clean up
+rm -rf build
+
+# Configure
+cmake -B build \
+-DWITH_Kokkos=OFF \
+
+#-DCMAKE_CXX_COMPILER=`which amdclang++` \
+#-DCMAKE_C_COMPILER=`which amdclang` \
+#-DAPEX_ROOT=$HOME/src/xpress-apex/install_gilgamesh_5.2.0
+
+# Build
+cmake --build build -j16
+ctest --test-dir build
