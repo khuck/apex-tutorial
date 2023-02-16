cc8bdd25ce6c21f7feae14c64c2bcea4597b0b37
Kevin Huck <khuck@cs.uoregon.edu>
2023-02-16 13:14:39 -0800

    Adding Kokkos hip support

--- amd.sh
+++ amd.sh
@@ -7,6 +7,14 @@ rm -rf build
 
 # Configure
 cmake -B build \
+-DKokkos_ENABLE_OPENMP=ON \
+-DKokkos_ENABLE_HIP=ON \
+-DKokkos_ENABLE_SERIAL=ON \
+-DKokkos_ARCH_ZEN2=ON \
+-DKokkos_ARCH_VEGA90A=ON \
+-DKokkos_ENABLE_AGGRESSIVE_VECTORIZATION=ON \
+-DKokkos_ENABLE_TESTS=OFF \
+
 
 #-DCMAKE_CXX_COMPILER=`which amdclang++` \
 #-DCMAKE_C_COMPILER=`which amdclang` \
