805dd47aa122dfd9a68eb035ca7f31c7e5e24e7b
Kevin Huck <khuck@cs.uoregon.edu>
2023-02-23 10:43:11 -0800

    Adding lumi script

---
+++ src/scripts/lumi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+export CXX=`which hipcc`
+export CC=`which amdclang`
+export APEX_ROOT=$HOME/install/apex
+
+# clean up
+rm -rf build
+
+# Configure
+cmake -B build \
+-DWITH_KokkosKernels=OFF \
+-DKokkos_ENABLE_OPENMP=ON \
+-DKokkos_ENABLE_HIP=ON \
+-DKokkos_ENABLE_SERIAL=ON \
+-DKokkos_ARCH_ZEN2=ON \
+-DKokkos_ARCH_VEGA90A=ON \
+-DKokkos_ENABLE_AGGRESSIVE_VECTORIZATION=ON \
+-DKokkosKernels_ENABLE_EXAMPLES=OFF \
+-DROCTX_ROOT=/opt/rocm-5.0.2/roctracer
+
+# Build
+cmake --build build -j16
+ctest --test-dir build
