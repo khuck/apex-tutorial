90178fc88455d855618b10a18a07a5dc962fe9d7
Kevin Huck <khuck@cs.uoregon.edu>
2023-02-23 10:21:24 -0800

    Limit max threads to 8 for examples

--- src/C++/std_thread/apex_deps_std_thread.cpp
+++ src/C++/std_thread/apex_deps_std_thread.cpp
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include "apex_api.hpp"
 
-const size_t nthreads{std::max<size_t>(8,std::thread::hardware_concurrency())};
+const size_t nthreads{std::min<size_t>(8,std::thread::hardware_concurrency())};
 
 void doWork(int scale = 1, std::shared_ptr<apex::task_wrapper> parent = nullptr) {
     auto task = apex::scoped_timer(__func__, parent);
