04305a4e9bb08fb116f930c7d4c79300107d6f83
Kevin Huck <khuck@cs.uoregon.edu>
2023-03-04 05:00:08 +0100

    updating simple script

--- src/scripts/simple.sh
+++ src/scripts/simple.sh
@@ -6,10 +6,9 @@ rm -rf build
 # Configure
 cmake -B build \
 -DWITH_Kokkos=OFF \
-
-#-DCMAKE_CXX_COMPILER=`which amdclang++` \
-#-DCMAKE_C_COMPILER=`which amdclang` \
-#-DAPEX_ROOT=$HOME/src/xpress-apex/install_gilgamesh_5.2.0
+-DCMAKE_CXX_COMPILER=`which clang++` \
+-DCMAKE_C_COMPILER=`which clang` \
+-DAPEX_ROOT=`spack location -i apex`
 
 # Build
 cmake --build build -j16
